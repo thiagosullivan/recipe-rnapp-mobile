45b8ad75efbf32d0af72feb4e38f0f3fb4fc51ef
thiagosullivan <thiagosullivan12@gmail.com>
2025-11-20 15:50:48 -0300

    feat: new assets: images, font and colors schema

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen. 123</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Edit app/index.tsx to edit this screen. 123
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "green",
+  },
+});
