ca3ed74c3535157ac1b2b131fbad936bd9a9014f
thiagosullivan <thiagosullivan12@gmail.com>
2025-11-22 05:19:44 -0300

    feat: verify email screen

---
+++ app/(auth)/verify-email.jsx
@@ -0,0 +1,109 @@
+import {
+  View,
+  Text,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { useSignUp } from "@clerk/clerk-expo";
+import { authStyles } from "../../assets/styles/auth.styles";
+import { Image } from "expo-image";
+import { COLORS } from "../../constants/colors";
+
+const VerifyEmail = ({ email, onBack }) => {
+  const { isLoaded, signUp, setActive } = useSignUp();
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleVerification = async () => {
+    if (!isLoaded) return;
+
+    setLoading(true);
+
+    try {
+      const signUpAttempt = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      if (signUpAttempt.status === "complete") {
+        await setActive({ session: signUpAttempt.createdSessionId });
+      } else {
+        Alert.alert("Error", "Verification failed. Please try again.");
+        console.error(JSON.stringify(signUpAttempt, null, 2));
+      }
+    } catch (error) {
+      Alert.alert(
+        "Error",
+        error.errors?.[0]?.message || "Failed to create account"
+      );
+      console.error(JSON.stringify(error, null, 2));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={authStyles.container}>
+      <KeyboardAvoidingView
+        style={authStyles.keyboardView}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+      >
+        <ScrollView
+          contentContainerStyle={authStyles.scrollContent}
+          showsVerticalScrollIndicator={false}
+        >
+          {/* Image container */}
+          <View style={authStyles.imageContainer}>
+            <Image
+              source={require("../../assets/images/i3.png")}
+              style={authStyles.image}
+              contentFit="contain"
+            />
+          </View>
+          <Text style={authStyles.title}>Verify Your Email</Text>
+          <Text style={authStyles.subtitle}>
+            We&apos;ve sent a verification code to {email}
+          </Text>
+
+          <View style={authStyles.formContainer}>
+            <TextInput
+              style={authStyles.textInput}
+              placeholder="Enter verification code"
+              placeholderTextColor={COLORS.textLight}
+              value={code}
+              onChangeText={setCode}
+              keyboardType="number-pad"
+              autoCapitalize="none"
+            />
+            <TouchableOpacity
+              style={[
+                authStyles.authButton,
+                loading && authStyles.buttonDisabled,
+              ]}
+              onPress={handleVerification}
+              disabled={loading}
+              activeOpacity={0.8}
+            >
+              <Text style={authStyles.buttonText}>
+                {loading ? "Verifying..." : "Verify Email"}
+              </Text>
+            </TouchableOpacity>
+            {/* Back to Sing Up */}
+            <TouchableOpacity style={authStyles.linkContainer} onPress={onBack}>
+              <Text style={authStyles.linkText}>
+                <Text style={authStyles.link}>Back to Sign Up</Text>
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </View>
+  );
+};
+
+export default VerifyEmail;
